ed1ebaa48d15e5d7f5e96dd3b6b9924d97e60c40
Zhunisali <zshanabek@gmail.com>
2020-09-21 17:21:43 +0600

    feat: add video player

--- posts/models.py
+++ posts/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 class Post(e_models.TimeStampedModel):
     title = models.CharField(max_length=255)
+    content = models.TextField()
     link = models.URLField(max_length=200)
     cover = models.ImageField(upload_to='posts/covers', null=True)
     video = models.FileField(upload_to='posts/videos',
                              max_length=100, null=True)
     set = models.ForeignKey(
         "sets.Set", on_delete=models.CASCADE, null=True, related_name='posts')
-    content = models.TextField()
 
     def __str__(self):
         return self.title
