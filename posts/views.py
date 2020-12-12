eeff88fa54fe985031c88a8ce94fa41a242416fb
Zhunisali <zshanabek@gmail.com>
2020-12-12 21:18:45 +0600

    refactor views

--- posts/views.py
+++ posts/views.py
@@ -7,20 +7,23 @@ class PostListView(ListView):
     model = Post
     template_name = 'posts/index.html'
 
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'posts/detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        posts = Post.objects.filter(set=context['post'].set).order_by('created')
+        posts = Post.objects.filter(
+            set=context['post'].set).order_by('created')
         context['sidebar'] = posts
         return context
+
 
 def search_post(request):
     if request.method == "POST":
         query_name = request.POST.get('title', None)
         if query_name:
             results = Post.objects.filter(title__contains=query_name)
-            return render(request, 'posts/post-search.html', {"results":results})
+            return render(request, 'posts/post-search.html', {"results": results})
     return render(request, 'posts/post-search.html')
